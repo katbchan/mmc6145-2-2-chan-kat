eff6b43ec867f4ddad8b5487b86d771027da4f31
Kat <katchan@ufl.edu>
2026-02-07 19:34:31 -0500

    modified App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import CardGame from "./components/cardGame";
+import Header from "./components/header";
+import Modal from "./components/modal";
+import { useTimer } from "./util/customHooks";
+
+export default function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [bestTime, setBestTime] = useState(null);
+  const [previousTime, setPreviousTime] = useState(null);
+
+  const {
+    time,
+    start: timerStart,
+    stop: timerStop,
+    reset: timerReset,
+  } = useTimer();
+
+  const handleGameStart = () => {
+    if (!gameStarted) {
+      timerReset(); 
+      timerStart();
+      setGameStarted(true);
+    }
+  };
+
+  const handleGameEnd = () => {
+    timerStop();
+    setGameStarted(false);
+    setPreviousTime(time);
+    if (bestTime === null || time < bestTime) {
+      setBestTime(time);
+    }
+  };
+
+  const cardTexts = [
+    "Bunny 🐰",
+    "Frog 🐸",
+    "Panda 🐼",
+    "Doggy 🐶",
+    "Kitty 😺",
+    "Duck 🦆",
+  ];
+
+  return (
+    <>
+      <Header
+        // add time, bestTime, previousTime props
+        openModal={() => setShowModal(true)}
+        time={time}
+        bestTime={bestTime}
+        previousTime={previousTime}
+      />
+      <CardGame
+        // add onGameStart, onGameEnd props
+        cardTexts={cardTexts}
+        onGameStart={handleGameStart}
+        onGameEnd={handleGameEnd}
+      />
+      <Modal isShown={showModal} close={() => setShowModal(false)} />
+    </>
+  );
+}
+
